7ca70a2055a27ba864b60631073a24ca96e8f7dc
saijaswanth <jaswanth4321sj@gmail.com>
2024-03-01 01:36:47 -0600

    update commit

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function Home() {
           </div>
         </div>
         <div className="col-md-6">
-          <img src={Welcome} alt="image" className="Welcome mt-1"></img>{" "}
+          <img src={Welcome} alt="image" className="Welcome mt-1" style={{ maxWidth: '85%' }}></img>{" "}
         </div>
       </div>
     </div>
