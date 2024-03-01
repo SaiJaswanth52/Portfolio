a2a2bfd08f093b3a98f5ac40d0d390cafd94b572
saijaswanth <jaswanth4321sj@gmail.com>
2024-03-01 12:11:47 -0600

    update commit

--- src/components/Academics.js
+++ src/components/Academics.js
@@ -99,7 +99,7 @@ export default function Academics() {
         {education ? 'Close Education Summary' : 'Education Summary'}
       </Button>} <br></br>
 
-      <a href='https://drive.google.com/file/d/1yn3bgaJNQR5eZvtNpmtyQe4hiomQqBtg/view?usp=sharing' target='_blank'> My-RESUME</a>
+      <a href='https://drive.google.com/file/d/1Da23Yiosjk-frk0vN8UI6ECINv1qM0Ys/view?usp=sharing' target='_blank'> My-RESUME</a>
 
     </div>
   );
